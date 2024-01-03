897fc4cf0b4e2e76e6700111da73a729a0a901c7
Kifah Hassan <26527136+kifahhassan@users.noreply.github.com>
2024-01-03 16:24:42 -0600

    succesfully finished exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let repeated = '';
+for (let i = 0; i < num; i++){
+    repeated += string;
+}
+if (num < 0) {
+    return "ERROR";
+}
+    return repeated;
+
 
 };
 

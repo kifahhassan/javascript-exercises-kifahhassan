18b4d68263a4afbaa35247549e1797d73a82a12f
Kifah Hassan <26527136+kifahhassan@users.noreply.github.com>
2024-01-05 18:32:35 -0600

    finished exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+let newArray = [];
 
+for (let i = 0; i < array.length; i++) {
+    if (!args.includes(array[i])) {
+        newArray.push(array[i]);
+    }
+}
+return newArray;
 };
+
+
+//create a new array inside function to input non items
+//create a function that goes through each item, if the item is not included in the args, then add to new array
+//return new array
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 04_removeFromArray/solution/removeFromArray-solution.js
+++ 04_removeFromArray/solution/removeFromArray-solution.js
@@ -17,6 +17,8 @@ const removeFromArray = function (array, ...args) {
   return newArray;
 };
 
+console.log(removeFromArray([1,2,3,4], 3));
+
 // A simpler, but more advanced way to do it is to use the 'filter' function,
 // which basically does what we did with the forEach above.
 

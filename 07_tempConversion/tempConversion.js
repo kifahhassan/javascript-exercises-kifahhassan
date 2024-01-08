671bfaf406dae3920c02ee5b3a45c68023bf3441
Kifah Hassan <26527136+kifahhassan@users.noreply.github.com>
2024-01-07 20:06:57 -0600

    finished exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let toCelsius = (temp - 32) * 0.55555556;
+
+  // if (toCelsius === 0.0) {
+  //   return 0;
+  // }
+
+  return parseFloat(toCelsius.toFixed(1));
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let toFarenheit = temp * 1.8 + 32;
+
+  // if (toFarenheit === 0.0) {
+  //   return 0
+  // }
+
+  return parseFloat(toFarenheit.toFixed(1));
 };
+
+console.log("To celsius: " + convertToCelsius(80));
+
+console.log("to farenheit: " + convertToFahrenheit(82.22));
+
 
 // Do not edit below this line
 module.exports = {
